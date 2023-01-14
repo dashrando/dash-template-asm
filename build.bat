d1f7821944cec07cb9b6ab194ee0a0121bae21f5
MassHesteria <brad.hester@gmail.com>
2023-01-14 13:53:44 -0600

    Updated Windows build script

--- build.bat
+++ build.bat
@@ -1,4 +1,18 @@
-del build\dash_working.sfc
-copy build\supermetroid.sfc ..\dash_working.sfc
-%~dp0bin\windows\asar.exe src\main.asm build\dash_working.sfc
-cmd /k
+@echo off
+
+set ROM=build\dash_working.sfc
+set PATCH=build\dash_working.bps
+set VANILLA=build\SuperMetroid.sfc
+
+if exist %ROM% del /Q %ROM%
+
+if not exist %VANILLA% (
+   echo Missing vanilla rom: %VANILLA%
+   pause
+   exit 1
+)
+
+copy %VANILLA% %ROM%
+bin\windows\asar.exe --symbols=wla src\main.asm %ROM%
+bin\windows\flips.exe --create --bps %VANILLA% %ROM% %PATCH%
+pause
