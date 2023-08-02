bdc48223b7a0d6998ebccb138dd964435f37782f
MassHesteria <brad.hester@gmail.com>
2023-08-02 07:22:31 -0500

    Added subarea comments

--- src/subareas.asm
+++ src/subareas.asm
@@ -27,10 +27,15 @@ DetermineSubArea:
         ADC.w RoomIndex : TAX
         LDA.l $DF0000,X
         PHA
+
+        ; Rooms copied from another area need put their original area
+        ; in the top 4-bits of the low byte. This is because there is
+        ; some special area-specifics graphics logic in the game.
         AND.w #$00F0 : BEQ +
                 LSR #4
                 STA.w FxAreaIndex
         +
+
         PLA
         AND.w #$000F : CMP.w SubAreaIndex : BEQ +
                 INC.w HUDDrawFlag
