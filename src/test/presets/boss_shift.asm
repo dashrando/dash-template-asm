1f8ad6ad2f59ebf373ef52f9c9b07675956dce1b
MassHesteria <brad.hester@gmail.com>
2023-12-02 08:26:04 -0600

    Added boss shuffle testing preset

--- src/test/presets/boss_shift.asm
+++ src/test/presets/boss_shift.asm
@@ -18,25 +18,25 @@ InitializeForTesting: {
         %setup_controller()
 
         ; Add equipment and beams
-        %add_items(!MorphingBall,!Bombs,!SpaceJump,!ScrewAttack,!GravitySuit,!VariaSuit)
-        %add_beams(!PlasmaBeam,!WaveBeam,!IceBeam,!SpazerBeam)
+        %add_items(!MorphingBall,!Bombs,!SpaceJump,!ScrewAttack,!GravitySuit,!VariaSuit,!XrayScope)
+        %add_beams(!PlasmaBeam,!WaveBeam,!IceBeam,!SpazerBeam,!ChargeBeam)
         %unequip_beams(!SpazerBeam)
 
         ; Add some ammo
-        LDA.w #50
+        LDA.w #80
         STA.w CurrentPBs : STA.w MaxPBs
         STA.w CurrentSupers : STA.w MaxSupers
-        LDA.w #40
+        LDA.w #100
         STA.w CurrentMissiles : STA.w MaxMissiles
 
         ; Make sure Zebes is awake
         LDA.l EventFlags : ORA.w #$0001 : STA.l EventFlags
 
         ; Kill bosses
-        LDA.l BossFlagsVanilla   : ORA.w #$0100 : STA.l BossFlagsVanilla ; Brinstar
-        LDA.l BossFlagsVanilla+2 : ORA.w #$0100 : STA.l BossFlagsVanilla+2 ; Wrecked Ship
-        LDA.l BossFlagsVanilla+4 : ORA.w #$0001 : STA.l BossFlagsVanilla+4 ; Maridia
-        LDA.l BossFlagsVanilla+2 : ORA.w #$0001 : STA.l BossFlagsVanilla+2 ; Norfair
+        ;LDA.l BossFlagsVanilla   : ORA.w #$0100 : STA.l BossFlagsVanilla ; Brinstar
+        ;LDA.l BossFlagsVanilla+2 : ORA.w #$0100 : STA.l BossFlagsVanilla+2 ; Wrecked Ship
+        ;LDA.l BossFlagsVanilla+4 : ORA.w #$0001 : STA.l BossFlagsVanilla+4 ; Maridia
+        ;LDA.l BossFlagsVanilla+2 : ORA.w #$0001 : STA.l BossFlagsVanilla+2 ; Norfair
 
         ; ---- Setup bosses ----
         pushpc
@@ -56,7 +56,7 @@ InitializeForTesting: {
         org DoorFrom<boss>InNorfair : dw DoorVectorToPreKraid
         endmacro
 
-        %BossRoundTrip(Ridley)
+        %BossRoundTrip(Kraid)
 
         ; Testing Kraid in LN
         ;org DoorFromKraidInBrinstar : dw DoorVectorToPreRidley
