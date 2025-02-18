be18d65ddcb4c63555c3a76c0107e3e5926f588a
cassidoxa <cassidymoen@gmx.com>
2025-02-18 18:09:13 -0500

    Refactor DASH item menu code Remove some redundant bank-crossing warnpcs in main.asm

--- src/main.asm
+++ src/main.asm
@@ -43,45 +43,36 @@ warnpc $80FFC0 ; SNES ROM Header
 
 org $81EF1A
 incsrc save.asm
-incsrc fileselect/fileselect.asm
-warnpc $828000
 
 org $82F900
-incsrc fileselect/gameoptions.asm
+incsrc main_menu.asm
 incsrc menu.asm
 incsrc subareas.asm
 incsrc roompatching.asm
 incsrc stats/stats_doors.asm
 incsrc doortransition.asm
-warnpc $838000
 
 org $84EFE0
 incsrc newplms.asm
 incsrc fanfare.asm
-warnpc $858000
 
 org $859643
 incsrc messageboxes.asm
-warnpc $868000
 
 org $87C964
 incsrc credits/credits_script.asm
-warnpc $888000
 
 org $89AEFD
 incsrc itemtiles.asm
-warnpc $8A8000
 
 org $8BF760
 incsrc credits/credits_scroll.asm
 incsrc credits/itempercentage.asm
-warnpc $8C8000
 
 org $8CE1E9+(32*10)
 incbin data/titlelogo.pal
 org $8CF3E9
 incsrc titlelogo.asm
-warnpc $8D8000
 
 org $8DC696
 incsrc data/logofadeobject.asm
@@ -89,14 +80,12 @@ incsrc data/logofadeobject.asm
 org $8EE600
 CreditsCharacters:
 incbin data/slash_char.bin
-warnpc $8F8000
 
 org $8FE9A0
 incsrc rooms.asm
 incsrc doors.asm
 incsrc music.asm
 incsrc plmlists.asm
-warnpc $908000
 
 org $90F63A
 incsrc newitems.asm
@@ -105,12 +94,10 @@ incsrc suits.asm
 incsrc beams.asm
 incsrc stats/stats_weapons.asm
 incsrc spritesomething.asm
-warnpc $918000
 
 org $99EE21
 DashLogoSprite:
 incbin data/logosprite.bin
-warnpc $9A8000
 
 ; 
 org $9AB200
@@ -122,7 +109,7 @@ incbin data/numbertiles_inverted.2bpp
 org $9AB542
 incbin data/superpbicons.2bpp
 org $9AB750
-incbin data/hudtiles_2.nes ; For the one tile after the "Energ" tile.
+incbin data/hudtiles_2.nes ; For the one tile after the "Energy" tile.
 org $9AB691
 incbin data/missileicon.2bpp
 
@@ -131,12 +118,15 @@ incbin data/hudtiles_icons.2bpp
 
 org $A0F7D3
 incsrc enemies.asm
-warnpc $A18000
 
 org $A2F498
 incsrc credits/credits_stats.asm
 incsrc stats/game_end.asm
 warnpc $A38000
+
+org $B6F200
+DashMenuTiles:
+incbin ../data/menu_tiles.bin
 
 org $CEB230
 incsrc roomedits.asm
@@ -146,7 +136,6 @@ org $DF8000
 incsrc tables.asm ; Keep this first
 incsrc roomtables.asm
 incsrc credits/credits_data.asm
-warnpc $E08000
 
 if !INTERFACE == 1
     incsrc interface.asm
--- src/menu.asm
+++ src/menu.asm
@@ -1,137 +1,61 @@
+;------------------------------------------------------------------------------
+; Menu
+;------------------------------------------------------------------------------
+; Code and data related to the pause menu.
+;------------------------------------------------------------------------------
+!DASH_ITEMS_BITMASK = $0221 ; Every bit for all custom items in DashItemsCollected/Equipped
+
 ; More or less equivalent to the vanilla item toggle code
 ; but reworked to support toggling Dash items too
-HandleMenuItemToggle: {
-   PHY
+HandleMenuItemToggle:
+        CPY.w #VanillaItemsEquipped
+        BNE .toggle
+                LDA.w $0000,X : BIT.w VanillaItemsCollected
+                BNE .toggle
+                        LDY.w #DashItemsEquipped
+                        INC.b $3A
+        .toggle:
+        LDA.w $0000,Y : BIT.w $0000,X : BNE .unequip
+                .equip:
+                LDA.w $0000,Y : ORA.w $0000,X : STA.w $0000,Y
+                PLY
+                JMP.w $B5B4
+        .unequip:
+        LDA.w $0000,X : EOR.w #$FFFF : STA.b $12
+        LDA.w $0000,Y : AND.b $12 : STA.w $0000,Y
+        JMP.w $B5DA
 
-   ; Not toggling suits/boots
-   CPY.w #VanillaItemsEquipped
-   BNE .toggle
+LoadMenuTileMapPointer:
+; In: $3A - DASH item if not 0
+        LDA.b $3A : BNE .dash_item
+                LDA.w $C044,X ; What we wrote over
+                BRA .done
+        .dash_item
+        LDA.w DASHTilemapPointerList,X
+        .done
+        STZ.b $3A
+RTS
 
-   ; Jump to toggle logic if a collected vanilla item is selected
-   LDA.w $0000,x
-   BIT.w VanillaItemsCollected
-   BNE .toggle
-
-   ; Toggle Dash items instead
-   LDY.w #DashItemsEquipped
-
-.toggle:
-   LDA.w $0000,y
-   BIT.w $0000,x
-   BNE .unequip
-
-.equip:
-   LDA.w $0000,y
-   ORA.w $0000,x
-   STA.w $0000,y
-
-   PLY
-   JMP.w $B5B4
-
-.unequip:
-   LDA.w $0000,x
-   EOR.w #$FFFF
-   STA.b $12
-   LDA.w $0000,y
-   AND.b $12
-   STA.w $0000,y
-
-   PLY
-   JMP.w $B5DA
-}
-
-IsMenuItemCollected: {
-   ; Simply return if the vanilla item is collected
-   BIT.w VanillaItemsCollected
-   BNE +
-
-   ; Check to see if the Dash item is collected
-   BIT.w DashItemsCollected
-+  RTS
-}
-
-IsMenuItemEquipped: {
-   ; Simply return if the vanilla item is equipped
-   BIT.w VanillaItemsEquipped
-   BEQ +
-   RTS
-
-   ; Move to the Dash item check if the vanilla item is not collected
-+  BIT.w VanillaItemsCollected
-   BEQ +
-
-   ; Clear the zero flag and return
-   SEP #$02
-   RTS
-
-   ; Check to see if the Dash item is equipped
-+  BIT.w DashItemsEquipped
-   RTS
-}
+DASHTilemapPointerList:
+dw $0000, $0000, DashSuitTilemaps, DashBootTilemaps
 
 ; This is called when moving the menu cursor to determine whether
 ; the cursor should be able to move to specific spot.
-CheckEquipmentBitmask: {
-   ; Compute the address of the equipment bitmask
-   PHA
-   TXA
-   CLC : ADC $01,s
-   STA $01,s
-   PLY
-
-   ; Simply return if the vanilla item is collected
-   LDA.w $0000,y
-   BIT.w VanillaItemsCollected
-   BNE +
-
-   ; Check to see if the Dash item is collected
-   BIT.w DashItemsCollected
-+  RTS
-}
-
-; TODO: Make this cleaner. Currently does a brute force check on
-; each vanilla item (Varia, Gravity, Space Jump) and uses the
-; Dash equivalent tilemaps when appropriate.
-SelectMenuTiles: {
-   ; Move to the next check if not selecting Varia Suit
-   CPX #$BF64
-   BNE +
-
-   ; Simply return if Varia Suit is collected
-   LDA #$0001 : BIT.w VanillaItemsCollected
-   BNE .ret
-
-   ; Use the Heat Shield tilemap instead
-   LDX.w #HeatShieldTileMap
-   BRA .ret
-
-   ; Move to the next check if not selecting Gravity Suit
-+  CPX #$BF76
-   BNE +
-
-   ; Simply return if Gravity Suit is collected
-   LDA #$0020 : BIT.w VanillaItemsCollected
-   BNE .ret
-
-   ; Use the Pressure Valve tilemap instead
-   LDX.w #PressureValveTileMap
-   BRA .ret
-
-   ; Move to the next check if not selecting Space Jump
-+  CPX #$BFE4
-   BNE .ret
-
-   ; Simply return if Space Jump is not collected
-   LDA #$0200 : BIT.w VanillaItemsCollected
-   BNE .ret
-
-   ; Use the Double Jump timemap instead
-   LDX.w #DoubleJumpTileMap
-
-.ret:
-   LDY.w #$0000
-   RTS
-}
+CheckEquipmentBitmask:
+        ; Compute the address of the equipment bitmask
+        PHA
+        TXA
+        CLC : ADC $01,s
+        STA $01,s
+        PLY
+        ; Simply return if the vanilla item is collected
+        LDA.w $0000,y
+        BIT.w VanillaItemsCollected
+        BNE .exit
+                ; Check to see if the Dash item is collected
+                BIT.w DashItemsCollected
+.exit
+RTS
 
 ; Loads the custom Dash tiles at $2D00 in VRAM
 LoadMenuTiles:
@@ -149,16 +73,13 @@ LoadMenuTiles:
 RTS
 
 
-; oHEAT SHIELD
-HeatShieldTileMap:
+HeatShieldTilemap:
 dw $08FF, $0AD0, $0AD1, $0AD2, $0AD3, $0AD4, $0AD5, $08D4, $08D4
 
-; oPRESSURE VALVE
-PressureValveTileMap:
+PressureValveTilemap:
 dw $08FF, $0AD6, $0AD7, $0AD8, $0AD9, $0ADA, $0ADB, $0ADC, $0ADD
 
-; oDOUBLE JUMP
-DoubleJumpTileMap:
+DoubleJumpTilemap:
 dw $08FF, $0ADE, $0ADF, $0AE0, $0AE1, $0AE2, $0AE3, $0AE4, $08D4
 
 SetHUDFlagMenu:
@@ -168,8 +89,38 @@ SetHUDFlagMenu:
         JSR.w MenuCheckButton ; What we wrote over
 RTS
 
-pushpc
-org $B6F200
-DashMenuTiles:
-incbin ../data/menu_tiles.bin
-pullpc
+;------------------------------------------------------------------------------
+
+CheckDashSuitsMenu:
+        LDX.w DashSuitTilemaps,Y
+        STA.b $3E
+        BRA CheckDashItemsMenu
+CheckDashBootsMenu:
+        LDX.w DashBootTilemaps,Y
+        STA.b $3E
+CheckDashItemsMenu:
+        BIT.w #!DASH_ITEMS_BITMASK : BNE .dash_item
+                .empty
+                LDX.w #$C01A
+                LDA.w #$0012 : STA.b $16
+                JSR.w CopyFromDPToTilemap
+                RTS
+        .dash_item
+        BIT.w DashItemsCollected : BEQ .empty
+        LDA.w #$0012  : STA.b $16
+        JSR.w CopyFromDPToTilemap
+        LDA.b $3E : BIT.w DashItemsEquipped : BNE .not_equipped
+                LDA.w #$0C00 : STA.b $12
+                LDA.w #$0012 : STA.b $16
+                JSR.w SetMenuTilePalettes
+        .not_equipped
+RTS
+
+; Pointers to equipment tilemaps. Resembles vanilla table at $82C088 in layout.
+DashSuitTilemaps:
+dw HeatShieldTilemap, PressureValveTilemap
+DashBootTilemaps:
+dw $0000, DoubleJumpTilemap
+
+EquipmentBitfieldPointers:
+dw $0000, BeamsEquipped, VanillaItemsEquipped, VanillaItemsEquipped, DashItemsEquipped
