a3d0efbe0e83855fb7122e588a6d5c723f0d6201
MassHesteria <brad.hester@gmail.com>
2026-02-11 07:44:17 -0600

    Proper fix for misalign Ridley in EM

--- src/doors.asm
+++ src/doors.asm
@@ -258,7 +258,7 @@ endmacro
 
 %RidleyVector(Brinstar,$01)
 %RidleyVector(WreckedShip,$01)
-%RidleyVector(Maridia,$01)
+%RidleyVector(Maridia,$00)
 
 ;--------------------------------------------------------------------
 ; Door Alignment Table
